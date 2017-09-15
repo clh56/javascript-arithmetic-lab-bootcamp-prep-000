3e0c1c933bd85a7c29fb83ef9707c51d1f90345e
ciara hudson <cchudson33@gmail.com>
2017-09-15 00:58:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var number = 5
+ 
+number++
+
+number--
+
+number += 3
+
+number -= 2
+
+number *= 10 
+
+number /= 5
+
+var number = 10
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+
+parseInt('2', 10)
+
+parseInt('2.2222', 10)
+
+parseInt('nonsense!', 10)
+
+parseFloat('80.123999')
